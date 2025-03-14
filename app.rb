60a46a028e0342e3db25963f2639729d049adeeb
tomball357 <tball@chicagobooth.edu>
2025-03-14 20:16:53 +0000

    base2

--- app.rb
+++ app.rb
@@ -5,28 +5,49 @@ require 'httparty'
 require 'json'
 
 
-
-require 'sinatra'
-require 'httparty'
-require 'json'
-
 BASE_URL = "https://api.exchangerate.host"
+API_KEY = ENV['exchange_key']
 
-get '/' do
-  "Welcome to the Currency Converter API!"
-end
-
-get '/currencies' do
+# Fetch currency list once and store it to minimize API calls
+def fetch_currencies
   response = HTTParty.get("#{BASE_URL}/list")
-  data = JSON.parse(response.body)
-  data["currencies"].to_json
+  if response.success?
+    data = JSON.parse(response.body)
+    return data["currencies"] || {}  # Extract currency symbols
+  else
+    return {}  # Fallback in case of an error
+  end
 end
 
-get '/convert/:from/:to' do
-  from_currency = params[:from]
-  to_currency = params[:to]
+CURRENCIES = fetch_currencies
+
+# Root route - Displays all available currencies
+get '/' do
+  erb :homepage, locals: { currencies: CURRENCIES }
+end
+
+# Currency symbol page - Displays conversion options for a specific currency
+get '/:currency' do
+  currency = params[:currency].upcase
+  halt 404 unless CURRENCIES.key?(currency)  # Ensure valid currency
+  erb :currency, locals: { currency: currency, currencies: CURRENCIES }
+end
+
+# Currency conversion page - Displays conversion rate between two currencies
+get '/:from_currency/:to_currency' do
+  from_currency = params[:from_currency].upcase
+  to_currency = params[:to_currency].upcase
+
+  halt 404 unless CURRENCIES.key?(from_currency) && CURRENCIES.key?(to_currency)
+
   response = HTTParty.get("#{BASE_URL}/convert", query: { from: from_currency, to: to_currency, amount: 1 })
-  data = JSON.parse(response.body)
-  rate = data["result"]
-  { from: from_currency, to: to_currency, rate: rate }.to_json
+  
+  if response.success?
+    data = JSON.parse(response.body)
+    rate = data["result"]
+  else
+    rate = "Unavailable"
+  end
+
+  erb :conversion, locals: { from_currency: from_currency, to_currency: to_currency, rate: rate }
 end
